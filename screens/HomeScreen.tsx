5452db2094f57d1e3e8b72af5ff5495266a937cf
Alexander <allex91@live.se>
2023-10-10 14:50:21 +0200

    Added Native Pager View to HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,31 @@
 import { StyleSheet, View } from "react-native";
+import PagerView from "react-native-pager-view";
 import { Text } from "react-native-paper";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Homescreen!</Text>
-    </View>
+    <PagerView style={styles.pagerView} initialPage={0}>
+      <View key="1">
+        <Text>First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+      <View key="3">
+        <Text>Third page</Text>
+      </View>
+      <View key="4">
+        <Text>Fourth page</Text>
+      </View>
+      <View key="5">
+        <Text>Fifth page</Text>
+      </View>
+    </PagerView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pagerView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
