77fbee29b296ab2f0d19d900c450f5812ae970fb
Jesper Svensson <jesper-svensson@hotmail.com>
2023-10-11 10:00:00 +0200

    Added a simple loginpage.

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -1,7 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 export type RootStackParamList = {
+  Login: undefined;
   Home: undefined;
 };
 
@@ -10,6 +12,7 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 export default function RootNavigator() {
   return (
     <RootStack.Navigator>
+      <RootStack.Screen name="Login" component={LoginScreen} />
       <RootStack.Screen name="Home" component={HomeScreen} />
     </RootStack.Navigator>
   );
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, Checkbox, Button, TextInput } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+
+export default function LoginScreen() {
+  const [checked, setChecked] = useState(false);
+  const navigation = useNavigation();
+
+  const toggleCheckbox = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.topText}>
+        Logga in för att se en app där man kan skriva sina hemligheter
+      </Text>
+
+      <View style={styles.inputsContainer}>
+        <TextInput label="Username" />
+        <TextInput label="Password" secureTextEntry />
+
+        <View style={styles.checkboxContainer}>
+          <Checkbox.Item
+            label="Remember me"
+            color="orange"
+            status={checked ? "checked" : "unchecked"}
+            onPress={toggleCheckbox}
+          />
+          <Text>Forgot Password</Text>
+        </View>
+      </View>
+
+      <Button
+        buttonColor="green"
+        mode="contained"
+        onPress={() => navigation.navigate("Home")} /// har lagt till detta så vi kan ta oss till startsidan.
+        style={styles.button}
+      >
+        Login
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  
+  topText: {
+    flex: 1,
+    margin: 16,
+    fontSize: 16,
+  },
+
+  inputsContainer: {
+    flex: 1,
+    width: "80%",
+  },
+
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button: {
+    marginBottom: 100,
+    width: "80%",
+  },
+});
