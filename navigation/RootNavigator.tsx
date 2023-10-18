c94ef1c49785212396a2906b7d9f6fdd522674e6
FredrikMolgaard <fredrikmollgard@gmail.com>
2023-10-18 15:48:36 +0200

    Added buttons for navigation to AddNewHousehold

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -1,16 +1,17 @@
-import { Entypo } from "@expo/vector-icons";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useRef, useState } from "react";
-import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
-import { Text, useTheme } from "react-native-paper";
+import { Entypo } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useRef, useState } from 'react';
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
 
-import PagerView from "react-native-pager-view";
-import HouseholdOverviewScreen from "../screens/HouseholdOverviewScreen";
-import HouseholdScreen from "../screens/HouseholdChoreScreen";
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
-import WelcomeScreen from "../screens/WelcomeScreen";
-import AddChoreScreen from "../screens/AddChoreScreen";
+import PagerView from 'react-native-pager-view';
+import AddChoreScreen from '../screens/AddChoreScreen';
+import AddNewHousehold from '../screens/AddHouseholdScreen';
+import HouseholdScreen from '../screens/HouseholdChoreScreen';
+import HouseholdOverviewScreen from '../screens/HouseholdOverviewScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 export type RootStackParamList = {
   Welcome: undefined;
@@ -19,13 +20,14 @@ export type RootStackParamList = {
   Hushållet: undefined;
   HouseholdOverview: undefined;
   AddNewChore: undefined;
+  AddNewHousehold: undefined;
 };
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
   const [currentPage, setCurrentPage] = useState(0);
-  const pageNames = ["Idag", "Förra Veckan"];
+  const pageNames = ['Idag', 'Förra Veckan'];
   const pagerRef = useRef<PagerView | null>(null);
   const theme = useTheme();
 
@@ -41,22 +43,23 @@ export default function RootNavigator() {
   };
 
   return (
-    <RootStack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
+    <RootStack.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
       <RootStack.Screen
         options={{
-          title: "Välkommen!",
+          title: 'Välkommen!',
         }}
-        name="HouseholdOverview"
+        name='HouseholdOverview'
         component={HouseholdOverviewScreen}
       />
-      <RootStack.Screen name="Welcome" component={WelcomeScreen} />
-      <RootStack.Screen name="Login" component={LoginScreen} />
-      <RootStack.Screen name="Register" component={RegisterScreen} />
-      <RootStack.Screen name="AddNewChore" component={AddChoreScreen} />
+      <RootStack.Screen name='Welcome' component={WelcomeScreen} />
+      <RootStack.Screen name='Login' component={LoginScreen} />
+      <RootStack.Screen name='Register' component={RegisterScreen} />
+      <RootStack.Screen name='AddNewChore' component={AddChoreScreen} />
+      <RootStack.Screen name='AddNewHousehold' component={AddNewHousehold} />
 
       {/*Start of HouseHold-Screen*/}
       <RootStack.Screen
-        name="Hushållet"
+        name='Hushållet'
         options={{
           header: () => (
             <SafeAreaView
@@ -71,7 +74,7 @@ export default function RootNavigator() {
                   onPress={() => handlePageChange(currentPage - 1)}
                 >
                   <Text style={styles.navText}>
-                    <Entypo name="chevron-thin-left" size={24} />
+                    <Entypo name='chevron-thin-left' size={24} />
                   </Text>
                 </TouchableOpacity>
                 <Text style={{ ...styles.navText }}>
@@ -81,7 +84,7 @@ export default function RootNavigator() {
                   onPress={() => handlePageChange(currentPage + 1)}
                 >
                   <Text style={styles.navText}>
-                    <Entypo name="chevron-thin-right" size={24} />
+                    <Entypo name='chevron-thin-right' size={24} />
                   </Text>
                 </TouchableOpacity>
               </View>
@@ -108,15 +111,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   screenName: {
-    textAlign: "center",
+    textAlign: 'center',
     paddingBottom: 10,
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   navBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     paddingHorizontal: 20,
   },
   navText: {
