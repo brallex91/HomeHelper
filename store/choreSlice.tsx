dcf7e49592bdc678e46a813fc4d510543f0f6d95
brallex91 <113627087+brallex91@users.noreply.github.com>
2023-10-23 10:14:06 +0200

    Removed the info-button from the chore-card and made the card clickable

--- store/choreSlice.tsx
+++ store/choreSlice.tsx
@@ -1,12 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Chore {
-  description: string;
-  energyLevel: string;
-  frequency: string;
-  // lastCompletedDate: Date;
-  dateCreated: Date;
+  id: string;
   name: string;
+  description: string;
+  frequency: number; // Antal dagar
+  energyLevel: number; // 1, 2, 4, 6 eller 8
+  lastCompletedDate?: Date; // Datum när sysslan senast slutfördes
+  dateCreated: Date; // Datum när sysslan skapades
 }
 
 export interface ChoresState {
--- store/houseHoldSlice.ts
+++ store/houseHoldSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Chore } from "./choreSlice";
 
 export interface Profile {
   id: string;
