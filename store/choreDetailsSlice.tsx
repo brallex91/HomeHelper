ed93403f00adb0a9f212d67ff9878c46af20f497
Jesper Svensson <jesper-svensson@hotmail.com>
2023-10-24 11:31:06 +0200

    Added choreCompleted logic

--- store/choreDetailsSlice.tsx
+++ store/choreDetailsSlice.tsx
@@ -2,15 +2,41 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const choreDetailsSlice = createSlice({
   name: "choreDetails",
-  initialState: { chore: { id: "", name: "", description: "", dateCreated: "", frequency: "", energyLevel: "" }, },
+  initialState: {
+    chore: {
+      id: "",
+      name: "",
+      description: "",
+      dateCreated: "",
+      frequency: "",
+      energyLevel: "",
+      lastCompleted: "",
+    },
+  },
   reducers: {
     setChoreDetails: (state, action) => {
       state.chore = action.payload;
     },
     updateChoreDetails: (state, action) => {
-      const { id, name, description, dateCreated, frequency, energyLevel } = action.payload;
+      const {
+        id,
+        name,
+        description,
+        dateCreated,
+        frequency,
+        energyLevel,
+        lastCompleted,
+      } = action.payload;
       if (state.chore && state.chore.id === id) {
-        state.chore = { ...state.chore, name, description, dateCreated, frequency, energyLevel };
+        state.chore = {
+          ...state.chore,
+          name,
+          description,
+          dateCreated,
+          frequency,
+          energyLevel,
+          lastCompleted,
+        };
       }
     },
   },
